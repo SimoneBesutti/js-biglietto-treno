9e7cbbc9de9e6f0369af7cda45eb4779006f4931
simo <simonebesutti18@gmail.com>
2019-07-11 15:47:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var saluto, percorso, etapasseggero;
+var saluto, percorso, etapasseggero, costoPercorso, scontoPercorso;
 
 saluto = "Benvenuto nella sezione shop ticket"
 alert(saluto);
@@ -7,8 +7,17 @@ percorso = prompt("Inserire il numero dei km da percorrere");
 
 etapasseggero = prompt("Inserire l'età del passeggero");
 
-document.writeln(Math.floor(percorso *0.21 * 1)); 
+costoPercorso = percorso * 0.21;
 
 if (etapasseggero < 18) {
-    Math.floor(percorso / 20);
+    scontoPercorso=costoPercorso*20/100;
+    costoPercorso = costoPercorso - scontoPercorso;
 }
+
+if (etapasseggero > 65 ) {
+    scontoPercorso=costoPercorso*40/100;
+    costoPercorso = costoPercorso - scontoPercorso;
+}
+
+document.getElementById('mio_id').innerHTML = "Il costo del percorso sarà di " + costoPercorso + " euro";
+
